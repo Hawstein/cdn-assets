4820042abfc22b220547aac4d3300dd746d200d3
Hawstein <tomhawstein@gmail.com>
2018-10-15 20:09:09 +0800

    add algocasts assets

---
+++ algocasts/javascripts/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function (){
+
+    $('#launch-buy-modal-plan40').on('click', function(e){
+        e.preventDefault();
+        $('#modal-buy-plan40').modal();
+    });
+    $('#launch-buy-modal-plan100').on('click', function(e){
+        e.preventDefault();
+        $('#modal-buy-plan100').modal();
+    });
+});
+
+
