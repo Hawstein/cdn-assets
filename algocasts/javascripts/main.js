ccebc7f6e61be85a35dda4ff5b30e29d21735d73
Hawstein <tomhawstein@gmail.com>
2019-05-11 14:34:59 +0800

    assets for algocasts 2.0

--- algocasts/javascripts/main.js
+++ algocasts/javascripts/main.js
@@ -1,13 +1,5 @@
 $(document).ready(function (){
 
-    $('#launch-buy-modal-plan40').on('click', function(e){
-        e.preventDefault();
-        $('#modal-buy-plan40').modal();
-    });
-    $('#launch-buy-modal-plan100').on('click', function(e){
-        e.preventDefault();
-        $('#modal-buy-plan100').modal();
-    });
 });
 
 
